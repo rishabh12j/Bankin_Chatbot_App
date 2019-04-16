17b25fd32edbf64f320e6f6f2a402536647e96ae
rishabh12j <43145074+rishabh12j@users.noreply.github.com>
2019-04-16 12:11:33 +0530

    Update Bank_FAQbot.py

--- Bank_FAQbot.py
+++ Bank_FAQbot.py
@@ -11,7 +11,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import LabelEncoder as LE
 from sklearn.svm import SVC
-nltk.download()
+nltk.download('punkt')
 client=discord.Client()
 token ="DdL5vskiAPcpfTYpKfMupsHiJpTj6sta"
 stemmer = LancasterStemmer()
